75b28c8c7b3162a871c396132738f1ed427fcf00
Harry Schmidt <goldibex@casetext.com>
2014-03-20 17:40:09 -0700

    Guarantee utf-8 bytes for request body in forwarding handler

--- app_fixture.py
+++ app_fixture.py
@@ -6,7 +6,7 @@ from libcasetext.flask_utils import handle_forwarding
 app = Flask(__name__)
 @app.route("/<path:p>", methods=["POST"])
 def test_handler(p):
-    return handle_forwarding(request.data, request, IRI(request.path))
+    return handle_forwarding(request.data.decode("utf-8"), request, IRI(request.path))
 
 if __name__ == "__main__":
     app.run(debug=True)

bf12349fe90a815d7124c79ada2091a949094d52
Richard Downe <richard@casetext.com>
2014-08-18 16:00:52 -0700

    purged all references to libcasetext from app_fixture.py. This also required creating a "fake" IRI class, which we can nuke once we finish decoupling these 2 things.

--- test/app_fixture.py
+++ test/app_fixture.py
@@ -1,7 +1,15 @@
 from flask import Flask, request
+from flask_http_forwarding.forwarding import handle_forwarding
 
-from libcasetext.iri import IRI
-from libcasetext.flask_utils import handle_forwarding
+class IRI(object):
+    def __init__(self, iri):
+        self.iri_str = iri
+
+    def manifestation_str(self):
+        return self.iri_str
+
+    def mime_type(self):
+        return "text/xml"
 
 app = Flask(__name__)
 @app.route("/<path:p>", methods=["POST"])
